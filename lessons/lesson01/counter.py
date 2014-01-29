c9f4d1275c328974f35828829fee26b4ba8e82f5
Michael Yung <mail@michaelyung.com>
2014-01-29 20:38:26 +0800

    Latest one from Wednesday night

--- lessons/lesson01/counter.py
+++ lessons/lesson01/counter.py
@@ -1,3 +1,5 @@
+# Use that python binary to execute this script
+#
 #!/usr/bin/python
 
 #
@@ -27,19 +29,24 @@ lines = sys.stdin.readlines()
 lines.pop(0)
 
 # For each line, find the numbers in the list.
+# The strip() command just to strip spaces before / after the split string, just in case
 #
 # I suppose I can use function isinstance to test it's string or int ?
 #
 
 for line in lines:
-   age = int(line.strip().split(',')[0])
-   total_age = total_age + age
-   impression = impression + int(line.strip().split(',')[2])
-   total_click = total_click + int(line.strip().split(',')[3])
-   if age > max_age:
+	
+	clean_line = line.strip().split(',')
+
+	age = int(clean_line[0])
+	total_age = total_age + age
+	impression = impression + int(clean_line[2])
+	total_click = total_click + int(clean_line[3])
+	
+	if age > max_age:
    		max_age = age
 
-   row = row + 1
+	row = row + 1
 
 # Print out the figures
 
@@ -51,8 +58,8 @@ print '      Total record : ', row
 print '===================='
 
 print '       Average Age : %.2f' % ( total_age / row )
-print 'Click through rate : %0.4f' % ( total_click / impression )
-print '     Oldest person : ', max_age
+print 'Click Through Rate : %0.4f' % ( total_click / impression )
+print '     Oldest Person : ', max_age
 
 # Write also to a textfile
 
@@ -66,7 +73,7 @@ file.write('       Total record :' + str(row) + '\n')
 file.write('=====================\n')
 
 file.write('        Average Age : %.2f' % ( total_age / row ) + '\n')
-file.write(' Click through rate : %0.4f' % ( total_click / impression ) + '\n')
-file.write('      Oldest person :' + str(max_age) + '\n')
+file.write(' Click Through Rate : %0.4f' % ( total_click / impression ) + '\n')
+file.write('      Oldest Person :' + str(max_age) + '\n')
 
 file.close()
