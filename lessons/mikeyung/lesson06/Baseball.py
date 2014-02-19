0bb75785b70084dc96572dd66396f027d33f7ed1
Michael Yung <mail@michaelyung.com>
2014-02-19 19:15:53 +0800

    Another version with height as well. Looks also good variable

--- lessons/mikeyung/lesson06/Baseball.py
+++ lessons/mikeyung/lesson06/Baseball.py
@@ -6,7 +6,7 @@ from sklearn import linear_model
 baseball = pd.read_csv('/Users/michaelyung/DS_HK_1/data/baseball.csv')
 
 lm = linear_model.LinearRegression()
-log_lm = linear_model.LinearRegression()
+# log_lm = linear_model.LinearRegression()
 
 hr = [ [x] for x in baseball['HR'].values]
 rbi = [ [x] for x in baseball['RBI'].values]
@@ -23,7 +23,7 @@ salary = [ [x] for x in baseball['salary'].values]
 
 lm.fit(yearid, salary)
 
-#log_lm.fit(log_salary, log_hr)
+# log_lm.fit(log_salary, log_hr)
 
 print "      Model Intercept: ", lm.intercept_
 print "    Model Coefficient: ", lm.coef_
@@ -33,15 +33,46 @@ print "    Model Coefficient: ", lm.coef_
 lm.predict(salary)
 baseball['predict'] = lm.predict(salary)
 
-#log_lm.predict(log_hr)
-#baseball['log_predict'] = np.exp(log_lm.predict(log_hr))
+# log_lm.predict(log_hr)
+# baseball['log_predict'] = np.exp(log_lm.predict(log_hr))
 
-plt.title("Scatter of yearID and the prediction")
-plt.scatter(baseball['yearID'], baseball['predict'])
+plt.title("Scatter of yearID and the salary")
+plt.scatter(baseball['yearID'], baseball['salary'])
 plt.show()
 
 plt.title("yearID, Prediction ...")
 plt.plot(yearid, lm.predict(salary))
 plt.show()
+
+"""
+Looks like recent years have higher salary
+"""
+
+
+lm_height = linear_model.LinearRegression()
+
+lm_height.fit(height, salary)
+
+# log_lm.fit(log_salary, log_hr)
+
+print "      Model Intercept: ", lm_height.intercept_
+print "    Model Coefficient: ", lm_height.coef_
+# print "  Log Model Intercept: ", log_lm.intercept_
+# rint "Log Model Coefficient: ", log_lm.coef_
+
+lm_height.predict(salary)
+baseball['predict'] = lm_height.predict(salary)
+
+plt.title("Scatter of height and the salary")
+plt.scatter(baseball['height'], baseball['salary'])
+plt.show()
+
+plt.title("height, Prediction ...")
+plt.plot(height, lm.predict(salary))
+plt.show()
+
+"""
+Looks like mid-height player have higher salary
+"""
 
 
