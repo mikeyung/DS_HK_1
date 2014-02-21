c4b7030c64eccf1394353bc9066f658dc8f8843c
Michael Yung <mail@michaelyung.com>
2014-02-21 19:04:25 +0800

    Latest commit with baseball stuff

--- lessons/mikeyung/lesson06/Baseball.py
+++ lessons/mikeyung/lesson06/Baseball.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 
 baseball = pd.read_csv('/Users/michaelyung/DS_HK_1/data/baseball.csv')
+#baseball = baseball.fillna(0)
 
 lm = linear_model.LinearRegression()
-# log_lm = linear_model.LinearRegression()
+#log_lm = linear_model.LinearRegression()
 
 hr = [ [x] for x in baseball['HR'].values]
 rbi = [ [x] for x in baseball['RBI'].values]
@@ -18,23 +19,24 @@ weight = [ [x] for x in baseball['weight'].values]
 yearid = [ [x] for x in baseball['yearID'].values]
 salary = [ [x] for x in baseball['salary'].values]
 
-# log_hr = np.log(baseball['HR'].values)
-# log_salary = [ [x] for x in np.log(baseball['salary'].values)]
+
+#log_yearid = np.log(baseball['yearID'].values)
+#log_salary = [ [x] for x in np.log(baseball['salary'].values)]
 
 lm.fit(yearid, salary)
 
-# log_lm.fit(log_salary, log_hr)
+#log_lm.fit(log_yearid, log_salary)
 
 print "      Model Intercept: ", lm.intercept_
 print "    Model Coefficient: ", lm.coef_
-# print "  Log Model Intercept: ", log_lm.intercept_
-# rint "Log Model Coefficient: ", log_lm.coef_
+#print "  Log Model Intercept: ", log_lm.intercept_
+#print "Log Model Coefficient: ", log_lm.coef_
 
 lm.predict(yearid)
 baseball['predict'] = lm.predict(yearid)
 
-# log_lm.predict(log_hr)
-# baseball['log_predict'] = np.exp(log_lm.predict(log_hr))
+#log_lm.predict(log_yearid)
+#baseball['log_predict'] = np.exp(log_lm.predict(log_yearid))
 
 plt.title("Scatter of yearID and the salary")
 plt.scatter(baseball['yearID'], baseball['salary'])
@@ -43,6 +45,11 @@ plt.show()
 plt.title("yearID, Prediction ...")
 plt.plot(yearid, lm.predict(yearid))
 plt.show()
+
+#plt.title("Log yearID, Prediction ...")
+#plt.plot(log_yearid, log_lm.predict(log_yearid))
+#plt.show()
+
 
 """
 Looks like recent years have higher salary
@@ -75,4 +82,29 @@ plt.show()
 Looks like taller player have higher salary
 """
 
+lm_weight = linear_model.LinearRegression()
+
+lm_weight.fit(weight, salary)
+
+# log_lm.fit(log_salary, log_hr)
+
+print "      Model Intercept: ", lm_weight.intercept_
+print "    Model Coefficient: ", lm_weight.coef_
+# print "  Log Model Intercept: ", log_lm.intercept_
+# rint "Log Model Coefficient: ", log_lm.coef_
+
+lm_weight.predict(weight)
+baseball['predict'] = lm_weight.predict(weight)
+
+plt.title("Scatter of weight and the salary")
+plt.scatter(baseball['weight'], baseball['salary'])
+plt.show()
+
+plt.title("weight, Prediction ...")
+plt.plot(weight, lm.predict(weight))
+plt.show()
+
+"""
+Looks like lighter player have higher salary
+"""
 
